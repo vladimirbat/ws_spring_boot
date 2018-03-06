cdf029dcaa1773c11cd875058d4847f3bd7e2b7a
Vladimir Bataller <vladimirbataller@yahoo.es>
2018-03-06 16:13:43 +0100

    ff

---
+++ A01_SpringBoot_HolaMundo/src/main/java/ejemplo/A01_MainAplicacion.java
@@ -0,0 +1,12 @@
+package ejemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Clase que inicia la aplicación autoconfigurada
+@SpringBootApplication
+public class A01_MainAplicacion {
+	public static void main(String[] args) {
+		SpringApplication.run(A01_MainAplicacion.class, args);
+	}
+}
