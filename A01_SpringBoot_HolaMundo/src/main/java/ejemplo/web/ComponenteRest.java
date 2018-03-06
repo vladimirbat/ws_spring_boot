f0ca9332cf6a30311a08c2bbb356ee8789b58122
Vladimir Bataller <vladimirbataller@yahoo.es>
2018-03-06 18:59:46 +0100

    d

--- A01_SpringBoot_HolaMundo/src/main/java/ejemplo/web/ComponenteRest.java
+++ A01_SpringBoot_HolaMundo/src/main/java/ejemplo/web/ComponenteRest.java
@@ -2,14 +2,18 @@ package ejemplo.web;
 
 import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ComponenteRest {
+	@Value("${nombre}")
+	private String nombre;
+	
 	@RequestMapping("/saludar")
 	String saludo() {
-		return "Hola Mundo";
+		return "Hola Mundo, desde " + this.nombre;
 	}
 	@RequestMapping("/hora")
 	String hora() {
