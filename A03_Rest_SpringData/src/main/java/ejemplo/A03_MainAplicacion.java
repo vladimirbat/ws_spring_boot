c6aa13e542d8268bcf1f03a48ec8dd87e61791b8
Vladimir Bataller <vladimirbataller@yahoo.es>
2018-03-06 19:04:49 +0100

    A03

---
+++ A03_Rest_SpringData/src/main/java/ejemplo/A03_MainAplicacion.java
@@ -0,0 +1,12 @@
+package ejemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Clase que inicia la aplicaci�n autoconfigurada
+@SpringBootApplication
+public class A03_MainAplicacion {
+	public static void main(String[] args) {
+		SpringApplication.run(A03_MainAplicacion.class, args);
+	}
+}
