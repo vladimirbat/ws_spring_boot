220d721cb552f4920f35af0f26ea99142c3b8f8c
Vladimir Bataller <vladimirbataller@yahoo.es>
2018-03-06 18:03:52 +0100

    s

--- A02_WebConServlet/src/main/java/com/example/demo/A02_MainAplicacion.java
+++ A02_WebConServlet/src/main/java/com/example/demo/A02_MainAplicacion.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class A02_MainAplicacion {
 
-//	public static void main(String[] args) {
-//		SpringApplication.run(A02_MainAplicacion.class, args);
-//	}
+	public static void main(String[] args) {
+		SpringApplication.run(A02_MainAplicacion.class, args);
+	}
 }
