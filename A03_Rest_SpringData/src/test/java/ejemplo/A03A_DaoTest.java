0b037b4a579234bf79b0aaf752494ff31c17da27
Vladimir Bataller <vladimirbataller@yahoo.es>
2018-03-06 19:48:38 +0100

    d

--- A03_Rest_SpringData/src/test/java/ejemplo/A03A_DaoTest.java
+++ A03_Rest_SpringData/src/test/java/ejemplo/A03A_DaoTest.java
@@ -15,20 +15,20 @@ import ejemplo.dao.ClientesDao;
 import ejemplo.entities.ClienteEntity;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest // Carga el contexto de Spring asociado a @SpringBootApplicacion
 public class A03A_DaoTest {
 	@Autowired
 	private ClientesDao clientesDao;
 	
 	@Test
 	public void buscarClavePrimaria() {
-		ClienteEntity cliente = clientesDao.getOne(1L);
-		System.out.println("Cliente getOne: " + cliente);
+//		ClienteEntity cliente = clientesDao.getOne(1L);
+//		System.out.println("Cliente getOne: " + cliente);
 		Optional<ClienteEntity> resul = clientesDao.findById(1L);
 		ClienteEntity cliente2 = resul.orElse(new ClienteEntity(0,"","",0));
 		System.out.println("Cliente findById: " + cliente2);
-		assertNotNull(cliente);
-		assertEquals(1L, cliente.getDni());
+		assertNotNull(cliente2);
+		assertEquals(1L, cliente2.getDni());
 	}
 	
 	
