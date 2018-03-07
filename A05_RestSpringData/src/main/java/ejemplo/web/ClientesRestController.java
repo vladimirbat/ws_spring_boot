7b15ff7bf835dee50c6825801071f911403791ee
Vladimir Bataller <vladimirbataller@yahoo.es>
2018-03-07 17:32:49 +0100

    Rest implementado

--- A05_RestSpringData/src/main/java/ejemplo/web/ClientesRestController.java
+++ A05_RestSpringData/src/main/java/ejemplo/web/ClientesRestController.java
@@ -3,6 +3,8 @@ package ejemplo.web;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,4 +20,18 @@ public class ClientesRestController {
 	public List<ClienteEntity> clientesAll(){
 		return clientesDao.findAll();
 	}
+	@RequestMapping(path="clientes",method=RequestMethod.POST)
+	public ClienteEntity insertarCliente(@RequestBody ClienteEntity nuevo){
+		return clientesDao.saveAndFlush(nuevo);
+	}
+	
+	@RequestMapping(path="clientes/{dni}",method=RequestMethod.GET)
+	public ClienteEntity buscarPorDni(@PathVariable Long dni){
+		return clientesDao.findById(dni).orElse(new ClienteEntity(0,"","",0));
+	}
+	@RequestMapping(path="clientes/findBy/{ape}",method=RequestMethod.GET)
+	public List<ClienteEntity> buscarPorApellidos(@PathVariable String ape){
+		return clientesDao.findClientesByApellidosContaining(ape);
+	}
+	
 }
